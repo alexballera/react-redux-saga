679874d7816ce7c6c2a616cfdf045203d04329bd
Alex Ballera <alexballera@gmail.com>
2020-04-04 13:33:55 -0300

    Creado componente home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import {Container, Typography} from '@material-ui/core'
+import React, { useState } from 'react'
+import {Container, Typography, Card, Grid, TextField, Button} from '@material-ui/core'
 
-export default () => (
+export default () => {
+	const [searchText, setSearchText] = useState('')
+
+	const handleSearchTextChange = event => {
+		setSearchText(event.target.value)
+	}
+	
+	const handleCleanTextClick = event => {
+		console.log('10')
+	}
+	const handleSearchTextClick = event => {
+		console.log('10')
+	}
+	return (
 	<Container>
-		<Typography variant="h4">
-			Home
-		</Typography>
+		<Card>
+			<Grid container>
+				<Grid>
+					<Typography variant="h4">
+						Bienvenido
+					</Typography>
+				</Grid>
+				<Grid>
+					<label>Icono</label>
+				</Grid>
+			</Grid>
+		</Card>
+		<TextField
+		value={searchText}
+		placeholder="Buscar..."
+		onChange={handleSearchTextChange}
+		/>
+		<Grid>
+			<Button variant="contained" onClick={handleCleanTextClick}>Limpiar</Button>
+			<Button variant="contained" onClick={handleSearchTextClick} color="primary" size="large">Buscar</Button>
+		</Grid>
 	</Container>
-)
+)}
